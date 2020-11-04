2934a331f4eb326dacc87e2460dbf0f06e0766a5
luhc228 <luhengchang228@gmail.com>
2020-11-04 13:21:50 +0800

    chore: remove lib

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { IFileEntity, IBabelOption, IOption } from './typing';
-declare function sylvanas(files: string[], option: IOption): IFileEntity[];
-declare namespace sylvanas {
-    var parseText: (text: string, option?: IBabelOption) => string;
-}
-export = sylvanas;
--- lib/ts2js.d.ts
+++
@@ -1,3 +0,0 @@
-import { IFileEntity, IBabelOption } from './typing';
-declare function ts2js(fileList: IFileEntity[], option?: IBabelOption): IFileEntity[];
-export default ts2js;
